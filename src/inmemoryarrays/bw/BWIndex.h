e5033d17a07f1e4a1eeb5d5198a242332e21624a
Yannis Chronis <chronis@cs.wisc.edu>
2021-08-16 19:17:31 +0300

    loadkey new?

--- src/inmemoryarrays/bw/BWIndex.h
+++ src/inmemoryarrays/bw/BWIndex.h
@@ -64,7 +64,7 @@ class BWIterator : public IndexIterator {
 class BWIndex : public Index {
  public:
 
-  BWIndex() {
+  BWIndex(int numrows) {
     index_ = new BwTreeType{true,
                                 KeyComparator1{1},
                                 KeyEqualityChecker1{1}};
